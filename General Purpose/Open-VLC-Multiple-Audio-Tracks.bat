b3b4d88a0dfd1802358dbcd87c4a82c6d81bcdbf
Albertus Cilliers <albertus.cilliers@gmail.com>
2023-10-16 19:26:36 +0200

    Updated VLC multiple audio track script

--- General Purpose/Open-VLC-Multiple-Audio-Tracks.bat
+++ General Purpose/Open-VLC-Multiple-Audio-Tracks.bat
@@ -1,4 +1,4 @@
 @REM Opens VLC and plays all audio tracks on a file simultaneosly.
 @REM You might need to change the VLC path if you have it installed somewhere differently.
-set "VLC_PATH=C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
-start "" "%VLC_PATH%" --sout-all --sout #display
+set "VLC_PATH=C:\Program Files (x86)\VideoLAN\VLC"
+start "" "%VLC_PATH%\vlc.exe" --sout-all --sout #display
