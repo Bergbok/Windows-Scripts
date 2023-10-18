02ceed84938175e72b33670c93446257ca7d0dae
Albertus Cilliers <albertus.cilliers@gmail.com>
2023-10-18 16:44:57 +0200

    Added audio device switching nircmd scripts

---
+++ NirSoft/NirCmd/Switch-To-Headphones.cmd
@@ -0,0 +1,4 @@
+@REM Get NirCMD here: https://www.nirsoft.net/utils/nircmd.html
+@REM Replace "MyHeadphones" with your headphones device name
+
+nircmd setdefaultsounddevice "MyHeadphones" 
---
+++ NirSoft/NirCmd/Switch-To-Speakers.cmd
@@ -0,0 +1,4 @@
+@REM Get NirCMD here: https://www.nirsoft.net/utils/nircmd.html
+@REM Replace "MySpeakers" with your speakers device name
+
+nircmd setdefaultsounddevice "MySpeakers"
