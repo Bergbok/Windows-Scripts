bd6cc50a5394d06f482f57ce37eeacb66bc8ddc8
Albertus Cilliers <albertus.cilliers@gmail.com>
2023-10-18 16:53:15 +0200

    Refactored NirCMD -> NirCmd

--- NirSoft/NirCmd/Switch-To-Speakers.cmd
+++ NirSoft/NirCmd/Switch-To-Speakers.cmd
@@ -1,4 +1,4 @@
-@REM Get NirCMD here: https://www.nirsoft.net/utils/nircmd.html
+@REM Get NirCmd here: https://www.nirsoft.net/utils/nircmd.html
 @REM Replace "MySpeakers" with your speakers device name
 
 nircmd setdefaultsounddevice "MySpeakers"
