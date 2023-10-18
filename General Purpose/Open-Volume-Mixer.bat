779caab1394348d71240e40df290f7bd0f367d62
Albertus Cilliers <albertus.cilliers@gmail.com>
2023-10-18 16:47:09 +0200

    Updated Open-Volume-Mixer.bat

--- General Purpose/Open-Volume-Mixer.bat
+++ General Purpose/Open-Volume-Mixer.bat
@@ -1,5 +1,6 @@
-cd C:\Windows\System32
-start SndVol.exe
+start %WINDIR%\System32\SndVol.exe
 
 @REM Focuses the window if you have nircmd installed
+@REM Get NirCMD here: https://www.nirsoft.net/utils/nircmd.html
+
 nircmd win active title "Volume Mixer"
