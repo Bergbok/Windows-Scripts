65a5da4243134df8f2ec9b30da57c1130718d462
Albertus Cilliers <albertus.cilliers@gmail.com>
2023-10-22 18:28:48 +0200

    Added Open-MegaBasterd.bat

---
+++ General Purpose/Open-MegaBasterd.bat
@@ -0,0 +1,5 @@
+@REM Place this here to open MegaBasterd from the start menu: %AppData%\Roaming\Microsoft\Windows\Start Menu\Programs
+@REM Change this to the path you have MegaBasterd.bat placed.
+set "megabasted_path=C:\Users\Albertus\Documents\Utilities\Downloaders\MegaBasterd"
+
+%megabasted_path%\MegaBasterd.bat
