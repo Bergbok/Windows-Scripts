0c72574901c6e302fb790a7af762b8420a9e6104
Albertus Cilliers <albertus.cilliers@gmail.com>
2023-10-24 11:06:39 +0200

    Moved NirCMD audio switchers

--- NirSoft/NirCmd/Switching audio devices (use SoundVolumeView instead)/Switch-To-Headphones.cmd
+++ NirSoft/NirCmd/Switching audio devices (use SoundVolumeView instead)/Switch-To-Headphones.cmd
@@ -1,4 +1,4 @@
-@REM Get NirCMD here: https://www.nirsoft.net/utils/nircmd.html
+@REM Get NirCmd here: https://www.nirsoft.net/utils/nircmd.html
 @REM Replace "MyHeadphones" with your headphones device name
 
 nircmd setdefaultsounddevice "MyHeadphones" 
