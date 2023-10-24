399404f7b38db04fb505882f141ad5dac56e4ebd
Albertus Cilliers <albertus.cilliers@gmail.com>
2023-10-24 20:39:02 +0200

    Changed echo -> Write-Output in Toggle-Block.ps1

--- Firewall Blockers/Steam/Toggle-Block.ps1
+++ Firewall Blockers/Steam/Toggle-Block.ps1
@@ -6,7 +6,7 @@
 
 if (Get-NetFirewallRule | Where-Object DisplayName -EQ "Block-Steam"){
     Get-NetFirewallRule | Where-Object DisplayName -eq 'Block-Steam' | Remove-NetFirewallRule 
-	echo "Unblocked Steam"
+	Write-Output "Unblocked Steam"
 }else{
     New-NetFirewallRule -Action block -Program 'C:\Program Files (x86)\Common Files\Steam\SteamService.exe' -Profile any -Direction Outbound -Displayname 'Block-Steam' 
 	New-NetFirewallRule -Action block -Program 'C:\Program Files (x86)\Steam\bin\cef\cef.win7x64\steamwebhelper.exe' -Profile any -Direction Outbound -Displayname 'Block-Steam' 
@@ -14,5 +14,5 @@ if (Get-NetFirewallRule | Where-Object DisplayName -EQ "Block-Steam"){
 	New-NetFirewallRule -Action block -Program 'C:\Program Files (x86)\Common Files\Steam\SteamService.exe' -Profile any -Direction Inbound -Displayname 'Block-Steam' 
 	New-NetFirewallRule -Action block -Program 'C:\Program Files (x86)\Steam\bin\cef\cef.win7x64\steamwebhelper.exe' -Profile any -Direction Inbound -Displayname 'Block-Steam' 
 	New-NetFirewallRule -Action block -Program 'C:\program files (x86)\steam\steam.exe' -Profile any -direction Inbound -Displayname 'Block-Steam'
-    echo "Blocked Steam"
+    Write-Output "Blocked Steam"
 }
